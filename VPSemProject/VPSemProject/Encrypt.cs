fcfd7b1e5403078632a4fd3fa56ede3a5967f5d6
iMujtabaSyed <36928225+iMujtabaSyed@users.noreply.github.com>
2018-05-13 22:44:33 +0500

    Add files via upload
    VP Project Till Encryption of text to image

--- VPSemProject/VPSemProject/Encrypt.cs
+++ VPSemProject/VPSemProject/Encrypt.cs
@@ -16,8 +16,48 @@ namespace VPSemProject
         {
             InitializeComponent();
         }
+        public string filepath;
 
         private void Encrypt_MdiChildActivate(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog obj = new OpenFileDialog();
+            obj.Filter= "Image files(*.bmp) | *.bmp";
+            obj.InitialDirectory = @"C:\";
+            if(obj.ShowDialog()==DialogResult.OK)
+            {
+                filepath = obj.FileName.ToString();
+                pictureBox1.ImageLocation = filepath;
+            }
+        }
+        private void EncryptMessage()
+        {
+            Bitmap img = new Bitmap(filepath);
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+                    if (i < 1 && j <textBox1.TextLength)
+                    {
+                        char text = Convert.ToChar(textBox1.Text.Substring(j, 1));
+                        int converted = Convert.ToInt32(text);
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, pixel.B,converted));
+                        MessageBox.Show("Done", "Confirmation");
+                    }
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EncryptMessage();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
